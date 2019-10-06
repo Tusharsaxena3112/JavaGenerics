256583b88b4c970fcd1d35937c1ef39c8eefed6b
Tusharsaxena3112 <tushar.saxena_cs18@gla.ac.in>
2019-10-06 22:20:30 +0530

    adding addItem method in GroceryList.java

--- src/arrayList/GroceryList.java
+++ src/arrayList/GroceryList.java
@@ -2,7 +2,16 @@ package arrayList;
 
 // ArrayLists are basically  the resizeable arrays.
 
+import java.util.ArrayList;
+
 public class GroceryList {
+    ArrayList<String> groceryList = new ArrayList<String>();
+
+    public void addItem(String item) {
+        groceryList.add(item);
+        System.out.println(item + " has been added int your list");
+    }
+
 
 
 }
