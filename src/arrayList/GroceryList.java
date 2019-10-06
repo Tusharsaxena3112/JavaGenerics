ab84b9051850b33bb563819f7f1c6d3c2d14bdf5
Tusharsaxena3112 <tushar.saxena_cs18@gla.ac.in>
2019-10-06 22:35:19 +0530

    adding other methods in GroceryList.java

--- src/arrayList/GroceryList.java
+++ src/arrayList/GroceryList.java
@@ -7,11 +7,27 @@ import java.util.ArrayList;
 public class GroceryList {
     ArrayList<String> groceryList = new ArrayList<String>();
 
-    public void addItem(String item) {
-        groceryList.add(item);
-        System.out.println(item + " has been added int your list");
+    public void showGroceryList() {
+        System.out.println(groceryList);
     }
 
+    public void addItem(String item) {
+        groceryList.add(item);
+        System.out.println(item + " has been added in your list");
+    }
 
+    public int findIndex(String item) {
+        return groceryList.indexOf(item);
+    }
+
+    public void removeItem(String item) {
+        groceryList.remove(findIndex(item));
+        System.out.println(groceryList);
+    }
+
+    public void modifyItem(String item, String newItem) {
+        groceryList.set(findIndex(item), newItem);
+        System.out.println("Your item has been modified");
+    }
 
 }
--- src/arrayList/Main.java
+++ src/arrayList/Main.java
@@ -1,4 +1,11 @@
 package arrayList;
 
 public class Main {
+    public static void main(String[] args) {
+        GroceryList gl = new GroceryList();
+        gl.addItem("Cheese");
+        gl.addItem("Samosa");
+        gl.showGroceryList();
+        gl.removeItem("Cheese");
+    }
 }
