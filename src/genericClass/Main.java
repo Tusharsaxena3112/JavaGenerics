20ba5c3d970855e71c01dc5a137dbbd9bf341748
Tusharsaxena3112 <tushar.saxena_cs18@gla.ac.in>
2019-10-10 22:03:14 +0530

    adding comments

--- src/genericClass/Main.java
+++ src/genericClass/Main.java
@@ -2,10 +2,9 @@ package genericClass;
 
 public class Main {
     public static void main(String[] args) {
-        ClassGeneric<String> obj1 = new ClassGeneric<String>("Tushar");
+        ClassGeneric<String> obj1 = new ClassGeneric<String>("Tushar"); //Creating object with String arguments
         System.out.println(obj1.getValue());
-        ClassGeneric<Integer> obj2 = new ClassGeneric<Integer>(18);
+        ClassGeneric<Integer> obj2 = new ClassGeneric<Integer>(18);// Creating object with Integer argument.
         System.out.println(obj2.getValue());
-
     }
 }
