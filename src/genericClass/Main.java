903c155381804d9dfce0536467c7affd01f39d6d
Tusharsaxena3112 <tushar.saxena_cs18@gla.ac.in>
2019-10-10 21:58:38 +0530

    adding Main.java adding object with String parameters

---
+++ src/genericClass/Main.java
@@ -0,0 +1,10 @@
+package genericClass;
+
+public class Main {
+    public static void main(String[] args) {
+        ClassGeneric<String> obj1 = new ClassGeneric<String>("Tushar");
+        System.out.println(obj1.getValue());
+        ClassGeneric<Integer> obj2 = new ClassGeneric<Integer>(18);
+
+    }
+}
